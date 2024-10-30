40300c14067c6365408a80b914a66785dfbacb16
Israa <Israa.K.Odeh@hotmail.com>
2024-10-30 02:52:35 +0200

    Add the SearchBar component

---
+++ src/components/SearchBar.js
@@ -0,0 +1,29 @@
+import searchGray from "../images/search-gray.png";
+import searchBlack from "../images/search-black.png";
+
+export default function SearchBar() {
+  return (
+    <div className="form__search-bar">
+      <img
+        src={searchGray}
+        alt="Gray magnifying glass icon representing search function"
+        className="form__search-icon--gray"
+        width="15"
+        height="15"
+      />
+      <img
+        src={searchBlack}
+        alt="Black magnifying glass icon representing search function"
+        className="form__search-icon--black"
+        width="15"
+        height="15"
+      />
+      <input
+        className="form__search-input"
+        type="search"
+        name="search"
+        placeholder="Find members"
+      />
+    </div>
+  );
+}
