01051814bc8750bfdee7e4aeb05590e71f8dc866
Israa <Israa.K.Odeh@hotmail.com>
2024-10-30 03:15:44 +0200

    Add the MemberPicker component

---
+++ src/components/MemberPicker.js
@@ -0,0 +1,20 @@
+import SearchBar from "./SearchBar";
+import MemberList from "./MemberList";
+import FormButtons from "./FormButtons";
+
+export default function MemberPicker() {
+  return (
+    <main>
+      <form className="form" action="https://httpbin.org/post" method="post">
+        <section
+          className="form__members-container"
+          aria-label="Members to choose from for the frontend development team, with a search bar to filter results"
+        >
+          <SearchBar />
+          <MemberList />
+          <FormButtons />
+        </section>
+      </form>
+    </main>
+  );
+}
